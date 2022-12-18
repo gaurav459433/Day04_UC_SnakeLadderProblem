a922fd3a5543b3daf93f84f853b2165b96d6a875
gaurav459433 <gaurav459433@gmail.com>
2022-12-19 00:08:39 +0530

    UC-1 Player Starting Position

--- SnakeLadderProblem.java
+++ SnakeLadderProblem.java
@@ -3,5 +3,8 @@ package com.bridgelabz.day04.snakeladder;
 public class SnakeLadderProblem {
     public static void main(String[] args) {
         System.out.println("Welcome To Snake & Ladder Simulator");
+        System.out.println();
+        int singlePlayer = 0;
+        System.out.println("Starting Position of Single Player is :" +singlePlayer);
     }
 }
