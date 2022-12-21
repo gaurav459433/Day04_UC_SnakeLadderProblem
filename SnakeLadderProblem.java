1ede6facea5089fb12f406a5fc8c016efea8196e
gaurav459433 <gaurav459433@gmail.com>
2022-12-21 12:34:53 +0530

    UC-3 Player NoPlay Ladder or Snake

--- SnakeLadderProblem.java
+++ SnakeLadderProblem.java
@@ -9,7 +9,22 @@ public class SnakeLadderProblem {
         System.out.println();
         int singlePlayer = 0;
         System.out.println("Starting Position of Single Player is :" +singlePlayer);
+        
         int die = random.nextInt(6)+1;
+        int a = random.nextInt(3);
+        System.out.println();
         System.out.println("Player Rolls The Die & Get a Number :" +die);
+        if (a == 1) {
+            singlePlayer += die;
+            System.out.println("Gets a Ladder & Player Moves ahead By Number Of Position :"  +singlePlayer);
+            System.out.println("Player Current Posotion :" +singlePlayer);
+        } else if (a == 2) {
+            singlePlayer -= die;
+            System.out.println("Bites By Snake & Player Moves Behind By Number Of Position :" +singlePlayer);
+            System.out.println("Player Current Posotion :" +singlePlayer);
+        }
+        else {
+            System.out.println("No Play - Player Stay At Same Position");
+        }
     }
 }
